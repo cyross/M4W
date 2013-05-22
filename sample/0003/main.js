70338d35b575fc3bf7f85527c8e1f13d4b4ecdc0
cyross <cyross@po.twin.ne.jp>
2013-05-22 22:46:47 +0900

    added sample 0003

---
+++ sample/0003/main.js
@@ -0,0 +1,113 @@
+$(document).ready(function(){
+  // アセットロード終了時の処理
+  var on_ready = function(assets){
+    // m4w実行対象のdivタグ
+    var $area = $("div#game");
+
+    // メインロジック
+    var main_logic = function(){
+      // 今回は何もしない
+    };
+
+    // 座標の更新
+    var update_pos = function(e){
+      var offset = this.offset();
+      var x = e.pageX - offset.left;
+      var y = e.pageY - offset.top;
+      var w = 640 - window.m4w.screen.sprites[0].image.width;
+      var h = 480 - window.m4w.screen.sprites[0].image.height;
+      
+      // 画像がはみ出ないように調整
+      // 今回、x,yが0以下の時は考慮する必要はない
+      if(x > w){ x = w; }
+      if(y > h){ y = h; }
+      window.m4w.input_vars.gameX = x;
+      window.m4w.input_vars.gameY = y;
+    }.bind($area);
+
+    // 座標の設定
+    var update_sprite = function(e){
+      window.m4w.screen.sprites[0].move({
+        x:window.m4w.input_vars.gameX,
+        y:window.m4w.input_vars.gameY
+      });
+    }
+
+    window.m4w.screen.sprites[0] = assets.sprite.s01; // わざとインデックスを付けて登録
+    window.m4w.main_logic = main_logic;
+    window.m4w.input_vars.pushed = false;
+
+    // マウスのボタンを押した時、移動を開始
+    $area.on("mousedown", function(e){
+      e.preventDefault();
+
+      // 右ボタンを押したときは対象外
+      if(e.button == 2){ return; }
+
+      // 画像の移動を許可
+      window.m4w.input_vars.pushed = true;
+
+      // マウスカーソルの非表示化
+      this.css("cursor", "url('img/empty_mouse_cursor.png')");
+
+      // 画像の切り替え
+      window.m4w.screen.sprites[0] = assets.sprite.s02;
+
+      // 位置の更新
+      update_pos(e);
+
+      // スプライトの移動
+      update_sprite(e);
+    }.bind($area));
+
+    // マウスカーソルが動いた時、位置を更新
+    $area.on("mousemove", function(e){
+      // ボタンを押していないときは何もしない
+      if(!window.m4w.input_vars.pushed){ return; }
+
+      e.preventDefault();
+
+      // 位置の更新
+      update_pos(e);
+
+      // スプライトの移動
+      update_sprite(e);
+    }.bind($area));
+
+    // マウスボタンを離した時と
+    //、マウスカーソルが外に出た時は移動させない
+    $area.on("mouseup mouseout",   function(e){
+      // ボタンを押していないときは何もしない
+      if(!window.m4w.input_vars.pushed){ return; }
+
+      // 画像の移動を禁止
+      window.m4w.input_vars.pushed = false;
+
+      // マウスカーソルをもとに戻す
+      this.css("cursor", "auto");
+
+      // 画像の切り替え
+      window.m4w.screen.sprites[0] = assets.sprite.s01;
+
+      // 現在の位置を切り替えたスプライトと共有
+      update_sprite(e);
+
+    }.bind($area));
+  }
+
+  // M4Wの初期化
+  var body = $("#game");
+  body.m4w();
+
+  // アセットのロード
+  AssetsLoader.load({
+    assets: [
+      {type: "sprite", id: "s01", src:"img/sampleimage.gif"}, // 通常の画像
+      {type: "sprite", id: "s02", src:"img/sampleimage2.gif"} // 押下時の画像
+    ],
+    success: on_ready
+  });
+  
+  // メインループの開始
+  window.m4w.main_loop();
+});
